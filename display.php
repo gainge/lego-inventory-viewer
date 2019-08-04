fe5b37782ee4be714315787e08a2d17b567a1831
Gibson Ainge <gainge@gmail.com>
2019-08-04 12:56:34 -0600

    made it purrtier

--- display.php
+++ display.php
@@ -4,15 +4,56 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
     <title>Inventory</title>
 </head>
 <body>
+    <?php 
+        function buildLegoComponent($count, $image) {
+            echo "<div class='item'>";
+            echo    "<img src='" . $image . "' />";
+            echo     "<p>" . $count . "</p>";
+            echo "</div>";
+        }
+    ?>
+
     <?php 
         include('./util.php');
         $fileName = isset($_GET[FILE_HEADER]) ? $_GET[FILE_HEADER] : 'DNE';
         $filePath = createFilePath($fileName);
 
-        echo $fileName;
+        if (is_file($filePath)) {
+            // Let's display the parts list!
+            $f = fopen($filePath, "r");
+
+
+            // Read the lines of the CSV file
+            // We'll kind of be assuming that it follows the format of that one site, sooo
+            // Write out the containing div
+            echo "<div class='container'>";
+
+            $row = -1;
+            while (($line = fgetcsv($f)) !== false) {
+                $row++;
+
+                if ($row == 0) continue;
+                if (sizeof($line) <= 1) break;
+
+                // otherwise, we write out the component
+
+                $count = $line[2];
+                $image = $line[7];
+
+                buildLegoComponent($count, $image);
+            }
+
+            // Write end of container div
+            echo "</div>";
+
+            fclose($f);
+        } else {
+            echo "Requested File Not Found";
+        }
 
     ?>
 
